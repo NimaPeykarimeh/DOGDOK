636b1ca1492089d4e48f13ef0e9023e125f09857
NimaPeykarimeh <nimapeykerimeh87@gmail.com>
2023-07-24 20:52:56 +0300

    silah ve gürültü

--- DOGDOK/Assets/Nima/Script/CharacterMovement.cs
+++ DOGDOK/Assets/Nima/Script/CharacterMovement.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
+[RequireComponent(typeof(NoiseMaker))]
 public class CharacterMovement : MonoBehaviour
 {
+    [Header("Movement")]
     public float movementSpeed;
     [SerializeField] float walkSpeed;
     [SerializeField] float runSpeed;
@@ -12,15 +14,17 @@ public class CharacterMovement : MonoBehaviour
     public  CharacterController characterController;
     public Vector3 mousePosition;
     [SerializeField] GameObject mainCamera;
-
+    NoiseMaker noiseMaker;
+    [SerializeField] float noiseRange;
+    [SerializeField] Transform noiseCenter;
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        noiseMaker = GetComponent<NoiseMaker>();
     }
 
-    
-    void Update()
+    void MovePlayer()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
@@ -29,15 +33,28 @@ public class CharacterMovement : MonoBehaviour
         move = Vector3.ClampMagnitude(move, 1f);
 
         characterController.Move(move * movementSpeed * Time.deltaTime);
+    }
+
+    void Update()
+    {
+        MovePlayer();
 
 
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             movementSpeed = runSpeed;
+            noiseRange = runSpeed;
         }
         else if (Input.GetKeyUp(KeyCode.LeftShift))
         {
             movementSpeed = walkSpeed;
+            noiseRange = walkSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            noiseMaker.MakeNoise(noiseRange, noiseCenter);
         }
     }
 
