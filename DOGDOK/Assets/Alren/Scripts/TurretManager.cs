4ba3a14c62ab485b0c8d7da407afffc05742c365
Alren54 <114315354+Alren54@users.noreply.github.com>
2023-07-27 18:49:48 +0300

    Ufak turret arayüzü ayarlamaları

--- DOGDOK/Assets/Alren/Scripts/TurretManager.cs
+++ DOGDOK/Assets/Alren/Scripts/TurretManager.cs
@@ -1,22 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 
 public class TurretManager : MonoBehaviour
 {
     [SerializeField] private GameObject TurretPanel;
+    private bool isOpen;
     // Start is called before the first frame update
     void Start()
     {
+        isOpen = false;
         TurretPanel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E))
+        if (!isOpen && Input.GetKeyDown(KeyCode.T)) //Açma
         {
+            isOpen = true;
             TurretPanel.SetActive(true);
+        }
+        else if(isOpen && Input.GetKeyDown(KeyCode.T)) //Kapama
+        {
+            isOpen = false;
+            TurretPanel.SetActive(false);
         }
     }
 }
