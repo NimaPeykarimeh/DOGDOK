1cd7d963eefc39ab97a66e9ed91ec5fd3a72da11
NimaPeykarimeh <nimapeykerimeh87@gmail.com>
2023-08-28 01:31:06 +0300

    ShootingChanged

--- DOGDOK/Assets/Nima/Script/Shooting.cs
+++ DOGDOK/Assets/Nima/Script/Shooting.cs
@@ -3,19 +3,140 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof (NoiseMaker))]
-
+[RequireComponent(typeof (AudioSource))]
+[RequireComponent(typeof (WeaponController))]
 public class Shooting : MonoBehaviour
 {
-    
+    [Header("References")]
     NoiseMaker noiseMaker;
+    WeaponController weaponController;
+    [Space]
+
+    GameObject player;
+    PlayerController playerController;
+    AimPlayer aimPlayer;
     [SerializeField] AmmoPooling ammoPooling;
     [SerializeField] GameObject enemyHitParticle;
     [SerializeField] LayerMask bulletHitLayer;
     [SerializeField] Transform shootingPoint;
+
+    [Header("Info")]
+
+    [SerializeField] bool isShooting;
+    [SerializeField] float fireInterval;
+    [SerializeField] float fireTimer;
+    [Header("WeaoponSettings")]
+
+    [SerializeField] int fireRate = 2;
+    [SerializeField] int damage = 5;
+    [SerializeField] float shootRange = 100;
+    [SerializeField] float noiseRange = 15.6f;
+    [SerializeField] bool isAutomatic;
+    [SerializeField] bool isPiercing = false;
+    [SerializeField] Transform shootPoint;
+
+    [Header("Acceleration")]
+    [SerializeField] bool isAccelerating;
+    [SerializeField] float accelerationDuration = 5;
+    [SerializeField] float maxAcceleration = 10;
+    [SerializeField] float accelerationTimer;
+    [SerializeField] float acceleration = 1;
+    [Header("Recoil")]
+    [SerializeField] float recoilTreshold = 0.1f;
+    [SerializeField] bool isRecoilingUp;
+    [SerializeField] bool isRecoilingDown;
+
+    [SerializeField] float maxRecoil = 0.3f;
+    [SerializeField] float recoilMagnitude = 1f;
+    [SerializeField] float recoilSpeed = 10f;
+    float currentRecoil;
+    float targetRecoil;
+    [Header("Sound")]
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] List<AudioClip> gunSounds;
+    [SerializeField] float minPitch = 0.8f;
+    [SerializeField] float maxPitch = 1.2f;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         noiseMaker = GetComponent<NoiseMaker>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerController = player.GetComponent<PlayerController>();
+        aimPlayer = player.GetComponent<AimPlayer>();
+        audioSource = GetComponent<AudioSource>();
+        weaponController = GetComponent<WeaponController>();
+        fireInterval = (1 / (float)fireRate);
+        fireTimer = fireInterval;
+    }
+
+    void Recoil()
+    {
+        isRecoilingUp = true;
+        targetRecoil = Mathf.Clamp(currentRecoil + recoilMagnitude, 0, maxRecoil);
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1") && playerController.currentState == PlayerController.PlayerStates.Combat)
+        {
+            isShooting = true;
+        }
+        else
+        {
+            isShooting = false;
+            accelerationTimer = 0;
+        }
+
+
+
+        if (isShooting && isAccelerating)
+        {
+            accelerationTimer += Time.deltaTime;
+            accelerationTimer = Mathf.Clamp(accelerationTimer, 0, accelerationDuration);
+
+            acceleration = Mathf.Lerp(1, maxAcceleration, accelerationTimer / accelerationDuration);
+        }
+        else
+        {
+            acceleration = 1;
+        }
+        fireInterval = (1 / (float)fireRate);
+        fireTimer += Time.deltaTime * acceleration;
+
+        if (isShooting && fireTimer >= fireInterval && weaponController.canShoot)
+        {
+            fireTimer = 0;
+            Recoil();
+
+            Shoot(shootPoint, playerController.aimPlayer.GetAimHitInfo(), shootRange, noiseRange, isPiercing, damage);
+            audioSource.pitch = Mathf.Lerp(minPitch, maxPitch, accelerationTimer / accelerationDuration);
+            audioSource.PlayOneShot(gunSounds[0]);
+
+        }
+
+        if (isRecoilingUp)
+        {
+            currentRecoil = Mathf.Lerp(currentRecoil, targetRecoil, recoilSpeed * Time.deltaTime);
+            aimPlayer.rotationX -= currentRecoil;
+            aimPlayer.rotationY += Random.Range(-currentRecoil, currentRecoil);
+            if (Mathf.Abs(currentRecoil - targetRecoil) < recoilTreshold)
+            {
+                isRecoilingUp = false;
+                isRecoilingDown = true;
+            }
+        }
+        else if (isRecoilingDown)
+        {
+            currentRecoil = Mathf.Lerp(currentRecoil, 0, recoilSpeed * Time.deltaTime);
+            aimPlayer.rotationX += currentRecoil;
+
+            if (currentRecoil < recoilTreshold)
+            {
+                currentRecoil = 0;
+                isRecoilingDown = false;
+            }
+        }
     }
 
     public void Shoot(Transform _shootingPoint,Vector3 _targetPosition, float _shootingRange,float _noiseRange,bool _isPiercing, int _damage)
--- DOGDOK/Assets/Nima/Script/WeaponController.cs
+++ DOGDOK/Assets/Nima/Script/WeaponController.cs
@@ -3,45 +3,9 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-[RequireComponent(typeof(Shooting))]
 public class WeaponController : MonoBehaviour
 {
 
-    [Header("Reference")]
-    [SerializeField] PlayerController playerController;
-    [SerializeField] Shooting shooting;
-    [SerializeField] AimPlayer aimPlayer;
-    [Space]
-
-    [SerializeField] bool isShooting;
-    [SerializeField] float fireInterval;
-    [SerializeField] float fireTimer;
-
-    [Header("WeaoponSettings")]
-    [SerializeField] int fireRate;
-    [SerializeField] int damage;
-    [SerializeField] float shootRange;
-    [SerializeField] float noiseRange;
-    [SerializeField] bool isAutomatic;
-    [SerializeField] bool isPiercing;
-    [SerializeField] Transform shootPoint;
-    
-    [Header("Acceleration")]
-    [SerializeField] bool isAccelerating;
-    [SerializeField] float accelerationDuration = 5;
-    [SerializeField] float maxAcceleration = 2;
-    [SerializeField] float accelerationTimer;
-    [SerializeField] float acceleration = 1;
-    [Header("Recoil")]
-    [SerializeField] float recoilTreshold = 0.1f;
-    [SerializeField] bool isRecoilingUp;
-    [SerializeField] bool isRecoilingDown;
-
-    [SerializeField] float maxRecoil = 0.3f;
-    [SerializeField] float recoilMagnitude = 1f;
-    [SerializeField] float recoilSpeed = 10f;
-    float currentRecoil;
-    float targetRecoil;
     [Header("Weapon Info")]
     public float generatingDuration = 0.5f;
     public float dissolvingDuration = 1f;
@@ -49,11 +13,6 @@ public class WeaponController : MonoBehaviour
     public Material weaponMaterial;
     public bool canShoot;
     public Weapon1 Weapon1;
-
-    [SerializeField] AudioSource audioSource;
-    [SerializeField] List<AudioClip> gunSounds;
-    [SerializeField] float minPitch;
-    [SerializeField] float maxPitch;
 
     public enum WeaponType
     {
@@ -65,79 +24,6 @@ public class WeaponController : MonoBehaviour
     private void Start()
     {
         weaponMaterial = GetComponent<MeshRenderer>().material;
-        shooting = GetComponent<Shooting>();
-        fireInterval = (1 / (float)fireRate);
-        fireTimer = fireInterval;
     }
 
-    void Recoil()
-    {
-        isRecoilingUp = true;
-        targetRecoil = Mathf.Clamp(currentRecoil +  recoilMagnitude,0,maxRecoil);
-    }
-
-    private void Update()
-    {
-        if (Input.GetButton("Fire1") && playerController.currentState == PlayerController.PlayerStates.Combat)
-        {
-            isShooting = true;
-        }
-        else
-        {
-            isShooting = false;
-            accelerationTimer = 0;
-        }
-
-
-
-        if (isShooting && isAccelerating)
-        {
-            accelerationTimer += Time.deltaTime;
-            accelerationTimer = Mathf.Clamp(accelerationTimer,0,accelerationDuration);
-
-            acceleration = Mathf.Lerp(1,maxAcceleration,accelerationTimer/accelerationDuration);
-        }
-        else
-        {
-            acceleration = 1;
-        }
-        fireInterval = (1 / (float)fireRate);
-        fireTimer += Time.deltaTime * acceleration;
-
-        if (isShooting && fireTimer >= fireInterval && canShoot)
-        {
-            fireTimer = 0;
-            Recoil();
-            
-            shooting.Shoot(shootPoint,playerController.aimPlayer.GetAimHitInfo(),shootRange,noiseRange,isPiercing,damage);
-            audioSource.pitch = Mathf.Lerp(minPitch,maxPitch,accelerationTimer/accelerationDuration);
-            audioSource.PlayOneShot(gunSounds[0]);
-
-        }
-
-        if (isRecoilingUp)
-        {
-            currentRecoil = Mathf.Lerp(currentRecoil, targetRecoil,recoilSpeed * Time.deltaTime);
-            aimPlayer.rotationX -= currentRecoil;
-            aimPlayer.rotationY += Random.Range(-currentRecoil, currentRecoil);
-            if ( Mathf.Abs(currentRecoil - targetRecoil) < recoilTreshold)
-            {
-                isRecoilingUp = false;
-                isRecoilingDown = true;
-            }
-        }
-        else if(isRecoilingDown)
-        {
-            currentRecoil = Mathf.Lerp(currentRecoil,0, recoilSpeed * Time.deltaTime);
-            aimPlayer.rotationX += currentRecoil;
-            
-            if (currentRecoil < recoilTreshold)
-            {
-                currentRecoil = 0;
-                isRecoilingDown = false;
-            }
-        }
-    }
-
-    
 }
