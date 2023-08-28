f64bc3ab52c5b55ceeb138bb36161fe7ee00d49c
Alren54 <114315354+Alren54@users.noreply.github.com>
2023-08-28 11:43:31 +0300

    Silah değiştirme

--- DOGDOK/Assets/Alren/Scripts/WeaponManager.cs
+++ DOGDOK/Assets/Alren/Scripts/WeaponManager.cs
@@ -15,13 +15,13 @@ public class WeaponManager : MonoBehaviour
     [HideInInspector] public WeaponController CurrentWeaponController;
     private Renderer currentRenderer;
 
-    private float currentAnimationValue = 1f;
-    private const float unsolvedValue = -0.1f;
-    private const float dissolvedValue = 1f;
+    private float currentAnimationValue = 1f; // silahýn bozulma/generate edilme durumunda hangi sayý deðerinde olduðu
+    private const float unsolvedValue = -0.1f; // silahýn generate olmasý için gerekli olan deðer
+    private const float dissolvedValue = 1f; // silahýn bozulmasý için gerekli olan deðer
     private float generatingDuration;
     private float dissolvingDuration;
 
-    private int currentWeaponIndex = 0;
+    private int currentWeaponIndex = 0; //Silah seçme index'i
 
     private bool isGenerating; //Cismin generatingDuration sürecinin içinde olmasý durumunda true
     private bool isDissolving; //Cismin dissolvingDuration sürecinin içinde olmasý durumunda true
@@ -32,7 +32,7 @@ public class WeaponManager : MonoBehaviour
         {
             Weapons.Add(transform.GetChild(i).gameObject);
         }
-        UpdateWeaponVisibility();
+        UpdateToNewWeapon();
     }
     void Start()
     {
@@ -41,22 +41,26 @@ public class WeaponManager : MonoBehaviour
 
     private void Update()
     {
-        if (isGenerating && AimPlayer.isAiming) 
+        if (isGenerating && AimPlayer.isAiming)  // niþan alýyorken ve silah yükleniyorken silahý generate et.
         {
             GenerateWeaponMaterial();
         }
-        else if (!AimPlayer.isAiming) 
+        else if (!AimPlayer.isAiming) // niþan almýyorken silahý boz
         {
             CurrentWeaponController.canShoot = false;
             isGenerating = false;
             isDissolving = true;
             DissolveWeaponMaterial();
         }
-        else if (!isGenerating) // && AimPlayer.isAiming
+        else if (!isGenerating) // && AimPlayer.isAiming -> Niþan alýyorken ama silah yüklenmiyorken, Eðer ki dissolve komutu verilmediyse 1. if koþuluna girmesi için isGenerating = TRUE yap (optimize deðil), yoksa silahý boz
         {
-            isGenerating = true;
+            if (!isDissolving)
+            {
+                isGenerating = true;
+            }
+            else DissolveWeaponMaterial();
         }
-        WeaponSelectionInput();
+        WeaponSelectionInput(); // Eðer ki silah deðiþme inputu girildiyse, silahý bozmaya baþla ve yeni yüklenecek olan silah index'ini bozulma bitene kadar aklýnda tut, bozulma bitince þuanki silahý güncelle
     }
 
     private void WeaponSelectionInput()
@@ -65,32 +69,63 @@ public class WeaponManager : MonoBehaviour
         if (scroll > 0)
         {
             currentRenderer.material.SetFloat("_Dissolve", 1);
-            SelectWeapon(currentWeaponIndex + 1);
+            currentWeaponIndex++;
+            CurrentWeaponController.canShoot = false;
+            isGenerating = false;
+            isDissolving = true;
+            DissolveWeaponMaterial();
+            //SelectWeapon(currentWeaponIndex + 1);
         }
         else if (scroll < 0)
         {
             currentRenderer.material.SetFloat("_Dissolve", 1);
-            SelectWeapon(currentWeaponIndex - 1);
+            currentWeaponIndex--;
+            CurrentWeaponController.canShoot = false;
+            isGenerating = false;
+            isDissolving = true;
+            DissolveWeaponMaterial();
+            //electWeapon(currentWeaponIndex - 1);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha1) && currentWeaponIndex != 0)
         {
             currentWeaponIndex = 0;
-            SelectWeapon(currentWeaponIndex);
+            CurrentWeaponController.canShoot = false;
+            isGenerating = false;
+            isDissolving = true;
+            DissolveWeaponMaterial();
+            //SelectWeapon(currentWeaponIndex);
         }
 
         else if (Input.GetKeyDown(KeyCode.Alpha2) && currentWeaponIndex != 1)
         {
             currentWeaponIndex = 1;
-            SelectWeapon(currentWeaponIndex);
+            CurrentWeaponController.canShoot = false;
+            isGenerating = false;
+            isDissolving = true;
+            DissolveWeaponMaterial();
+            //SelectWeapon(currentWeaponIndex);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3) && currentWeaponIndex != 2)
         {
             currentWeaponIndex = 2;
-            SelectWeapon(currentWeaponIndex);
+            CurrentWeaponController.canShoot = false;
+            isGenerating = false;
+            isDissolving = true;
+            DissolveWeaponMaterial();
+            //SelectWeapon(currentWeaponIndex);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha4) && currentWeaponIndex != 3)
         {
             currentWeaponIndex = 3;
+            CurrentWeaponController.canShoot = false;
+            isGenerating = false;
+            isDissolving = true;
+            DissolveWeaponMaterial();
+            //SelectWeapon(currentWeaponIndex);
+        }
+
+        if(currentAnimationValue == 1f) // Silah tamamen görünmez olduysa silahý deðiþ.
+        {
             SelectWeapon(currentWeaponIndex);
         }
     }
@@ -101,10 +136,10 @@ public class WeaponManager : MonoBehaviour
 
         currentWeaponIndex = (nextWeaponIndex + Weapons.Count) % Weapons.Count;
 
-        UpdateWeaponVisibility();
+        UpdateToNewWeapon();
     }
 
-    private void UpdateWeaponVisibility()
+    private void UpdateToNewWeapon()  // Yeni Silahýn tüm gerekli deðerlerini al.
     {
         CurrentWeaponObject = Weapons[currentWeaponIndex];
         CurrentWeaponObject.SetActive(true);
@@ -117,32 +152,27 @@ public class WeaponManager : MonoBehaviour
         generatingDuration = CurrentWeaponController.generatingDuration;
         dissolvingDuration = CurrentWeaponController.dissolvingDuration;
 
-        if (AimPlayer.isAiming)
-        {
-            currentRenderer.material.SetFloat("_Dissolve", 1);
-            currentAnimationValue = 1;
-        }
-        else currentRenderer.material.SetFloat("_Dissolve", currentAnimationValue);
+        currentRenderer.material.SetFloat("_Dissolve", currentAnimationValue);
         
         isGenerating = true;
     }
-    private void GenerateWeaponMaterial()
+    private void GenerateWeaponMaterial() // isGenerating == true olduðu durumda gelir. Silahý görünür hale getirir.
     {
         currentAnimationValue = Mathf.MoveTowards(currentAnimationValue, unsolvedValue, (1 / generatingDuration) * Time.deltaTime);
         currentRenderer.material.SetFloat("_Dissolve", currentAnimationValue);
-        if (currentAnimationValue == unsolvedValue)
+        if (currentAnimationValue == unsolvedValue) // Generating bitince bool'u deðiþtirir, ateþ etmeye izin verir.
         {
             isGenerating = false;
             CurrentWeaponController.canShoot = true;
         }
-        else CurrentWeaponController.canShoot = false;
+        else CurrentWeaponController.canShoot = false; // Yoksa ateþ etmeye izin vermez.
     }
 
-    private void DissolveWeaponMaterial()
+    private void DissolveWeaponMaterial() // isDissolving == true olduðu durumda gelir. Silahý görünmez hale getirir.
     {
         currentAnimationValue = Mathf.MoveTowards(currentAnimationValue, dissolvedValue, (1 / dissolvingDuration) * Time.deltaTime);
         currentRenderer.material.SetFloat("_Dissolve", currentAnimationValue);
-        if (currentAnimationValue == dissolvedValue)
+        if (currentAnimationValue == dissolvedValue) // çözünme bitince bool'u deðiþtirir.
         {
             isDissolving = false;
         }
